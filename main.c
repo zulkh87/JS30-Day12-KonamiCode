0825704bff69cc105e14733e3ccfad88ff8e2d6e
Zulkh87-AF-K <156850017+zulkh87@users.noreply.github.com>
2024-05-23 01:42:31 +0300

    printf and format specifiers

--- main.c
+++ main.c
@@ -3,9 +3,18 @@
 
 int main()
 {
-    char characterName[] = "yoso";
-    int characterAge = 32;
-    printf("There was once a man named %s\n", characterName);
-    printf("He was %d years old", characterAge);
+    // basic data types begin
+    int age = 40;
+    double gpa = 3.7;
+    char grade = "A";
+    // basic data types end
+    char phrase[] = "One more soul to the call";
+    // printf function
+    printf("Yo \"World\"\n");
+    // format specifiers begin
+    printf("My favorite %s is %d\n", "number" ,910);
+    printf("%f is a decimal number\n", 87.034235);
+    // format specifiers end
+    // printf function ends
     return 0;
 }
